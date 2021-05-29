cae792305f43cf21fe5ffac047bf33c88a7d743d
Melih AKAY <iletisim@melihakay.com>
2021-05-30 01:18:22 +0300

    rehber başlangıç

---
+++ Rehber/ana.py
@@ -0,0 +1,105 @@
+import mysql.connector
+from mysql.connector import connect, Error
+from os import system, name
+from getpass import getpass
+from time import sleep
+
+def ekle():
+    print("İsim: ",end="")
+    isim = input()
+    print("Soyisim: ",end="")
+    soyisim = input()
+    print("Yaş: ",end="")
+    yas = input()
+    print("Meslek: ",end="")
+    meslek = input()
+
+    #en uzun stringi alan fonksiyon
+    liste = [isim , soyisim, yas, meslek]
+    max_len = -1
+    for n in liste:
+        if len(n) > max_len:
+            max_len = len(n)
+            en_uzun = n
+
+    #Tablo oluşturan fonksiyon
+    temizle()
+    print("  ","-"*(16 + len(en_uzun)))
+    print("  ","|","İsim:   ","|",isim," "*(len(en_uzun)-len(isim)),"|")
+    print("  ","-"*(16 + len(en_uzun)))
+    print("  ","|","Soyisim:","|",soyisim," "*(len(en_uzun)-len(soyisim)),"|")
+    print("  ","-"*(16 + len(en_uzun)))
+    print("  ","|","Yaş:    ","|",yas," "*(len(en_uzun)-len(yas)),"|")
+    print("  ","-"*(16 + len(en_uzun)))
+    print("  ","|","Meslek: ","|",meslek," "*(len(en_uzun)-len(meslek)),"|")
+    print("  ","-"*(16 + len(en_uzun)))
+
+    kesinlik = input("\nBelirtilen bilgileri rehbere ekle? (E/h)")
+    if kesinlik == "E":
+        print("Ekleniyor")
+        girkomut = """
+INSERT INTO genel
+(isim, soyisim, yas, meslek)
+VALUES ( %s, %s, %s, %s )
+"""
+        kayit = [
+            (isim, soyisim, yas, meslek),]
+        with connection.cursor() as cursor:
+            cursor.executemany(girkomut, kayit)
+            connection.commit()
+        print("Eklendi!")
+        sleep(3)
+def temizle():
+    #windows
+    if name == 'nt':
+        _ = system('cls')
+
+    #linux
+    else:
+        _ = system('clear')
+    print("")
+def baglan():
+    try:
+        with connect(
+            host="melihakay.com",
+            user="py3",
+            password=getpass("Şifre: "),
+            database=("rehber"),
+            ) as connection:
+            print("Başarılı Giriş! Lütfen Bekleyin...")
+            print(connection)
+            sleep(2)
+            temizle()
+    except Error as e:
+            print("Başarısız Giriş!: \n", e)
+            exit()
+def cikis():
+    temizle()
+    print("2sn içinde çıkış")
+    sleep(2)
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+    quit(0)
+
+
+
+#Giriş
+temizle()
+print("Rehber (version 0.2.1)")
+baglan()
+
+while True:
+    temizle()
+    isaret = input("Rehbere verilecek komut? (Yardım için \'yardim\', çıkış için \'bitti\')\n>>")
+    if isaret == "bitti":
+        cikis()
+    elif isaret == "yardim":
+        print("""
+Çıkış için bitti,
+Eklemek için ekle,
+        """)
+    elif isaret == "ekle":
+        temizle()
+        ekle()
